7e50c919cc82624240f6a25fa191482f9386487e
LucaTröstl <lucsky.cr7@gmail.com>
2024-10-08 11:44:30 +0200

    Added 2 test methods: testCalculate Area, testCalculateCircumference.
    There were no failures to be detected when the radius is put to 0,
    however this is an issue. A circles Area can't be detected if it has no
    radius.
    --> Subject to change (If the Circle has a radius of 0 it can not be
    created)

--- src/formsTesting/CircleTesting.java
+++ src/formsTesting/CircleTesting.java
@@ -7,10 +7,47 @@ import org.testng.annotations.Test;
 public class CircleTesting {
 	
   @Test
-  public void testCalculateArea() {
+  /**
+   * Method to test the method "testCalculateArea"
+   * The circle in this example has the radius of 5 --> works fine
+   */
+  public void testCalculateArea1() {
 	 Circle c = new Circle(2, 3, 5 ); // 2 and 3 are the coordinates where the circle is placed
 	 double result = c.calculateArea();
 	 Assert.assertEquals(result, c.getRadius()*c.getRadius()*Math.PI); // 78.53 is the correct awnser
+  }
+  
+  @Test
+  /**
+   * Method to test the method "testCalculateArea"
+   * The circle in this example has the radius of 0 --> works fine
+   */
+  public void testCalculateArea2() {
+	 Circle c = new Circle(2, 3, 0 ); // 2 and 3 are the coordinates where the circle is placed
+	 double result = c.calculateArea();
+	 Assert.assertEquals(result, c.getRadius()*c.getRadius()*Math.PI); 
+  }
+  
+  @Test
+  /**
+   * Method to test the method "CalculateCircumference"
+   * The circle in this example has the radius of 4 --> works fine
+   */
+  public void testCalculateCircumference1() {
+	  Circle c = new Circle(1, 2, 4);
+	  double result = c.calculateCircumference();
+	  Assert.assertEquals(result, 2*c.getRadius()*Math.PI);
+  }
+  
+  @Test
+  /**
+   * Method to test the method "CalculateCircumference"
+   * The circle in this example has the radius of 0 --> works fine
+   */
+  public void testCalculateCircumference2() {
+	  Circle c = new Circle(1, 2, 0);
+	  double result = c.calculateCircumference();
+	  Assert.assertEquals(result, 2*c.getRadius()*Math.PI);
   }
   
 }
