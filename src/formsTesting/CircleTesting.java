9fb73aee3353fbcc634154de1c668ac3e0ab2103
LucaTröstl <lucsky.cr7@gmail.com>
2024-10-08 11:32:22 +0200

    Tested the method CalculateArea

--- src/formsTesting/CircleTesting.java
+++ src/formsTesting/CircleTesting.java
@@ -1,9 +1,16 @@
 package formsTesting;
 
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CircleTesting {
+	
   @Test
-  public void f() {
+  public void testCalculateArea() {
+	 Circle c = new Circle(2, 3, 5 ); // 2 and 3 are the coordinates where the circle is placed
+	 double result = c.calculateArea();
+	 Assert.assertEquals(result, c.getRadius()*c.getRadius()*Math.PI); // 78.53 is the correct awnser
   }
+  
 }
