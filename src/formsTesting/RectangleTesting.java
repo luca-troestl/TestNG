923de1db036adf7095eafe5ea042cbd71b4dfe08
LucaTröstl <lucsky.cr7@gmail.com>
2024-10-21 16:03:19 +0200

    Added testing methods for rectangle

--- src/formsTesting/RectangleTesting.java
+++ src/formsTesting/RectangleTesting.java
@@ -1,9 +1,36 @@
 package formsTesting;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RectangleTesting {
-  @Test
-  public void f() {
-  }
+  
+	@Test
+	public void testCalculateCircumferenceRectangle1() throws LengthZeroException {
+		  Rectangle r = new Rectangle(1, 2, 4, 5);
+		  double result = r.calculateCircumference();
+		  Assert.assertEquals(result, (r.getLength()*2)+(r.getWidth()*2)); // Result should be 18 --> works fine
+	  }
+	
+	@Test
+	public void testCalculateCircumferenceRectangle2() throws LengthZeroException {
+		  Rectangle r = new Rectangle(1, 2, 4, 0);
+		  double result = r.calculateCircumference();
+		  Assert.assertEquals(result, (r.getLength()*2)+(r.getWidth()*2)); // Should throw an exception because one length is zero
+	  }
+	
+	@Test
+	public void testCalculateAreaRectangle1() throws LengthZeroException {
+		  Rectangle r = new Rectangle(1, 2, 4, 2);
+		  double result = r.calculateArea();
+		  Assert.assertEquals(result, r.getLength()*r.getWidth());// Result should be 8 --> works fine
+	}
+	
+	@Test
+	public void testCalculateAreaRectangle2() throws LengthZeroException {
+		  Rectangle r = new Rectangle(1, 2, 4, 0);
+		  double result = r.calculateArea();
+		  Assert.assertEquals(result, r.getLength()*r.getWidth());// Should throw an exception because one length is zero
+	}
+	
 }
