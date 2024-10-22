55f94facd3c3e323564504522dccbffd51863224
LucaTröstl <lucsky.cr7@gmail.com>
2024-10-22 08:23:20 +0200

    Added methods to the square testing class and tested them. Works fine!

--- src/formsTesting/Square.java
+++ src/formsTesting/Square.java
@@ -2,32 +2,40 @@ package formsTesting;
 
 public class Square extends Form {
 	
-	private Rectangle rectangle;
+	private double width;
 	
 	public Square(double x, double y, double width) {
 		super(x, y);
-		rectangle = new Rectangle(x,y,width,3*width);
+		this.width = width;
 	}
 
-	public Rectangle getRectangle() {
-		return rectangle;
+	public double getWidth() {
+		return width;
 	}
 
-	public void setRectangle(Rectangle rectangle) {
-		this.rectangle = rectangle;
+	public void setWidth(double width) {
+		this.width = width;
 	}
 
-	public double calculateArea() {
-		return this.rectangle.calculateArea();
+	public double calculateArea() throws LengthZeroException{
+		if(this.width <= 0) {
+			throw new LengthZeroException("Die Länge oder Breite darf nicht 0 sein wenn die Fläche berechnet wird!");
+		} else {
+		return this.width*this.width;
+		}
 	}
 
-	public double calculateCircumference() {
-		return this.rectangle.calculateCircumference();
+	public double calculateCircumference() throws LengthZeroException{
+		if(this.width <= 0) {
+			throw new LengthZeroException("Die Länge oder Breite darf nicht 0 sein wenn der Umfang berechnet wird!");
+		} else {
+		return this.width*4;
+		}
 	}
 	
 	public String toString() {
 		return "Square (" + this.getX() + "|" + this.getY() + ") with width = " 
-					+ this.rectangle.getWidth();
+				+ this.getWidth(); 
 	}
 	
 
