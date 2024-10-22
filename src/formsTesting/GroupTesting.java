b826ab599a2bb2f8de8237103211168796ccbe6c
LucaTröstl <lucsky.cr7@gmail.com>
2024-10-22 10:56:55 +0200

    Added a few methods to GroupTesting

--- src/formsTesting/GroupTesting.java
+++ src/formsTesting/GroupTesting.java
@@ -1,9 +1,124 @@
 package formsTesting;
-
+ 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
+ 
+ 
 public class GroupTesting {
-  @Test
-  public void f() {
-  }
+ 
+     /**
+     * Tests adding a square form to the group and checking if it was added correctly.
+     */
+    @Test
+    public void testAddForm() throws LengthZeroException {
+        Form f = new Square(1, 1, 1);
+        Group g = new Group();
+        g.addForm(f);
+        Assert.assertEquals(f, g.getForms().get(0));
+    }
+ 
+     /**
+     * Tests adding a rectangle form to the group and checking if it was added correctly.
+     */
+    @Test
+    public void testAddForm2() throws LengthZeroException {
+        Form f = new Rectangle(2, 9, 4, 8);
+        Group g = new Group();
+        g.addForm(f);
+        Assert.assertEquals(f, g.getForms().get(0));
+    }
+ 
+     /**
+     * Tests the area calculation of multiple forms added to the group.
+     */
+    @Test
+    public void testCalculateArea() throws LengthZeroException {
+        Form f = new Rectangle(2, 9, 4, 8);
+        Form f2 = new Circle(2, 9, 8);
+        Form f3 = new Circle(2, 9, 1);
+        Group g = new Group();
+        g.addForm(f);
+        g.addForm(f2);
+        g.addForm(f3);
+        double result = g.calculateArea();
+        Assert.assertEquals(result, f.calculateArea() + f2.calculateArea() + f3.calculateArea());
+    }
+ 
+     /**
+     * Tests area calculation for a group of forms including rectangle, square, and circle.
+     */
+    @Test
+    public void testCalculateArea2() throws LengthZeroException {
+        Form f = new Rectangle(2, 9, 4, 45);
+        Form f2 = new Square(2, 11, 2);
+        Form f3 = new Circle(2, 9, 33);
+        Group g = new Group();
+        g.addForm(f);
+        g.addForm(f2);
+        g.addForm(f3);
+        double result = g.calculateArea();
+        Assert.assertEquals(result, f.calculateArea() + f2.calculateArea() + f3.calculateArea());
+    }
+ 
+     /**
+     * Tests the circumference calculation of multiple forms added to the group.
+     */
+    @Test
+    public void testCalculateCurcumference() throws LengthZeroException {
+        Form f = new Rectangle(2, 9, 4, 8);
+        Form f2 = new Circle(2, 9, 8);
+        Form f3 = new Circle(2, 9, 1);
+        Group g = new Group();
+        g.addForm(f);
+        g.addForm(f2);
+        g.addForm(f3);
+        double result = g.calculateCircumference();
+        Assert.assertEquals(result, f.calculateCircumference() + f2.calculateCircumference() + f3.calculateCircumference());
+    }
+ 
+     /**
+     * Tests circumference calculation for a group with rectangle, square, and circle.
+     */
+    @Test
+    public void testCalculateCircumference2() throws LengthZeroException {
+        Form f = new Rectangle(2, 9, 4, 45);
+        Form f2 = new Square(2, 11, 2);
+        Form f3 = new Circle(2, 9, 33);
+        Group g = new Group();
+        g.addForm(f);
+        g.addForm(f2);
+        g.addForm(f3);
+        double result = g.calculateCircumference();
+        Assert.assertEquals(result, f.calculateCircumference() + f2.calculateCircumference() + f3.calculateCircumference());
+    }
+ 
+     /**
+     * DIe Methode testet ob deine Fette schiache Mutter einen 40cm Dildo deepthroaten kann.
+     */
+    @Test
+    public void testFindBiggestForm() throws LengthZeroException {
+        Form f = new Rectangle(2, 9, 4, 45);
+        Form f2 = new Square(2, 11, 2);
+        Form f3 = new Circle(2, 9, 3);
+        Group g = new Group();
+        g.addForm(f);
+        g.addForm(f2);
+        g.addForm(f3);
+        Assert.assertEquals(g.findBiggestForm(), f);
+    }
+ 
+     /**
+     * Tests finding the biggest form in the group when a square has the largest area.
+     */
+    @Test
+    public void testFindBiggestForm2() throws LengthZeroException {
+        Form f = new Rectangle(2, 9, 4, 4);
+        Form f2 = new Square(2, 11, 34);
+        Form f3 = new Circle(2, 9, 3);
+        Group g = new Group();
+        g.addForm(f);
+        g.addForm(f2);
+        g.addForm(f3);
+        Assert.assertEquals(g.findBiggestForm(), f2);
+    }
 }
