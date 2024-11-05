ec565a5433b44e581036c3f9b7e19b948842e4f8
StringManu <manuel.scheiber@hak-reutte.ac.at>
2024-11-05 11:43:42 +0100

    Aufgabe 4 erster Test

--- src/exceptionsTesting/Test_Aufgabe4.java
+++ src/exceptionsTesting/Test_Aufgabe4.java
@@ -1,10 +1,19 @@
 package exceptionsTesting;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test_Aufgabe4 {
   @Test
   public void testReadFile() throws FileNotFoundException{
-	  
+	 Aufgabe4 a = new Aufgabe4();
+	 a.readFile("LALA");
   }
+  
+  @Test
+  public void testReadFile2() throws FileNotFoundException{
+	 Aufgabe4 a = new Aufgabe4();
+	 a.readFile("C:\\hallo.txt");
+  }
+  
 }
