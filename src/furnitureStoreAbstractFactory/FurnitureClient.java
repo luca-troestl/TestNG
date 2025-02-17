6e29a0316818e62485ffc65a7932aa38e0088ffe
LucaTröstl <lucsky.cr7@gmail.com>
2025-02-17 17:10:58 +0100

    Finished excercise

--- src/furnitureStoreAbstractFactory/FurnitureClient.java
+++ src/furnitureStoreAbstractFactory/FurnitureClient.java
@@ -1,10 +1,41 @@
 package furnitureStoreAbstractFactory;
 
+
 public class FurnitureClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+private FurnitureFactory factory;
+	
+	public FurnitureClient(FurnitureFactory factory) {
+		this.factory = factory;
+	}
+	
+	public FurnitureClient(FurnitureType type) {
+		this.factory = FurnitureFactory.getFactory(type);
+	}
+	
+	public void orderFurniture() {
+		Chairs chair = factory.createChair();
+		Table table = factory.createTable();
+		chair.orderItem();
+		table.orderItem();
+	}
+	
+	public FurnitureFactory getFactory() {
+		return factory;
+	}
 
+	public void setFactory(FurnitureFactory factory) {
+		this.factory = factory;
+	}
+
+	public static void main(String[] args) {
+		FurnitureClient modernFactory = new FurnitureClient(FurnitureType.MODERN);
+		modernFactory.orderFurniture();
+		
+		FurnitureFactory factory = FurnitureFactory.getFactory(FurnitureType.ANTIQUE);
+		Chairs chair = factory.createChair();
+		chair.orderItem();
+		
 	}
 
 }
