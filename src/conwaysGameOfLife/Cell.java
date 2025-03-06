1f4dbc08cbf3a66e8f674dc367ad9adc77d94526
LucaTröstl <lucsky.cr7@gmail.com>
2025-03-06 13:00:53 +0100

    Added a few methods to cell class

--- src/conwaysGameOfLife/Cell.java
+++ src/conwaysGameOfLife/Cell.java
@@ -1,12 +1,23 @@
 package conwaysGameOfLife;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cell {
 	
 	private boolean isAlive;
-	private boolean isDead;
-	private List<Cell> neighbour;
+	private boolean willLive;
+	private List<Cell> neighbours;
+	
+	/*
+	 * This Method creats a dead cell, it is not alive. Also it creates
+	 * a new ArrayList for the upcoming cells
+	 */
+	public Cell() {
+		this.isAlive = false;
+		this.willLive = false;
+		this.neighbours = new ArrayList<>();
+	}
 	
 	
 
